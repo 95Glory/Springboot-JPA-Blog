024178dd49f082a181c5acf68a467a2c11f0a90a
Young Kwang Kim <dudrhkd6550@naver.com>
2022-07-25 23:20:59 +0900

    환경세팅 완료

---
+++ src/test/java/com/example/metablog/MetablogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetablogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
